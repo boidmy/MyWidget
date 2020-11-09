46c03ff4d5451130e034b569cf32ee40f210254a
rilflatron1428 <flatron1428@gmail.com>
2020-11-09 16:07:48 +0900

    불필요 파일 제거

--- app/src/main/java/com/mywidget/viewModel/MainFragmentRvViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mywidget.viewModel
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class MainFragmentRvViewModel(application: Application) : AndroidViewModel(application) {
-
-}
--- app/src/main/java/com/mywidget/viewModel/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mywidget.viewModel
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class MainViewModel(application: Application) : AndroidViewModel(application) {
-
-}
