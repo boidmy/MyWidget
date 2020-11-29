7e88707a50c90966e7a49ee280f37623d0d49daf
rilflatron1428 <flatron1428@gmail.com>
2020-11-30 00:21:33 +0900

    invite 팝업 databinding 생성

--- app/src/main/java/com/mywidget/extension/BindingAdapter.kt
+++ app/src/main/java/com/mywidget/extension/BindingAdapter.kt
@@ -57,31 +57,6 @@ fun termProcessing(textView: TextView?, data: String?) {
     text(textView, value)
 }
 
-@BindingAdapter("memoTxt", "dateTxt", "viewModel")
-fun memoOnclick(button: Button, memo: EditText, date: TextView, viewModel: MainFragmentViewModel) {
-    button.setOnClickListener {
-        AlertDialog.Builder(button.context)
-            .setTitle("아싸~")
-            .setMessage("♥입력됐대용♥")
-            .setIcon(android.R.drawable.ic_menu_save)
-            .setPositiveButton("yes") { _, _ ->
-                date.tag?.let {
-                    viewModel.insertMemo(memo.text.toString(), it.toString())
-                    Toast.makeText(button.context, "저장했대요!!", Toast.LENGTH_SHORT).show()
-                } ?: run {
-                    Toast.makeText(button.context, "날짜를 선택해주세요!", Toast.LENGTH_SHORT).show()
-                }
-            }
-            .setNegativeButton(
-                android.R.string.no
-            ) { _, _ ->
-                // 취소시 처리 로직
-                Toast.makeText(button.context, "취소했대요ㅠㅠ.", Toast.LENGTH_SHORT).show()
-            }.show()
-        viewModel.dialogVisible.value = false
-    }
-}
-
 @BindingAdapter("NowDate")
 fun setNowDate(view: View, textView: TextView) {
     view.setOnClickListener {
--- app/src/main/java/com/mywidget/extension/BindingAdapterOnClick.kt
+++ app/src/main/java/com/mywidget/extension/BindingAdapterOnClick.kt
@@ -4,6 +4,7 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.mywidget.ui.signup.SignUpActivity
